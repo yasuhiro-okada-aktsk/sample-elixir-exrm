755abee8b7ee64dc82679d09c800d301393063ab
Yas Okada <yasuhiro.okada@aktsk.jp>
2016-05-25 12:11:57 +0900

    added apex

--- mix.exs
+++ mix.exs
@@ -37,7 +37,8 @@ defmodule SampleElixirExrm.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.9"},
      {:cowboy, "~> 1.0"},
-     {:exrm, "~> 1.0"}
+     {:exrm, "~> 1.0"},
+     {:apex, "~>0.5.0"}
      ]
   end
 
--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,9 @@ defmodule SampleElixirExrm.PageController do
   use SampleElixirExrm.Web, :controller
 
   def index(conn, _params) do
+    data = [false, 42, ~w(forty two), [time: "now"], %{foo: :bar}]
+    Apex.ap data
+
     render conn, "index.html"
   end
 end
